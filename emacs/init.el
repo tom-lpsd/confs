e8ca66dfdff07264fd116a2379a512c9855ebdec
Tom Tsuruhara <tom.lpsd@gmail.com>
2010-08-05 01:48:25 +0900

    separete miscs and added packup-inhibited setting

--- emacs/init.el
+++ emacs/init.el
@@ -20,32 +20,3 @@
 
 (when (load (expand-file-name "~/.emacs.d/elpa/package.el"))
   (package-initialize))
-
-;;; 以下 Emacs テクニックバイブル p.59 より
-
-;;; gcを減らす
-(setq gc-cons-threshold (* 10 gc-cons-threshold))
-
-;;; ログの記録行数を増やす
-(setq message-log-max 10000)
-
-;;; ミニバッファを再帰的に呼び出せるようにする
-(setq enable-recursive-minibuffers t)
-
-;;; ダイアログボックスを使わないようにする
-(setq use-dialog-box nil)
-(defalias 'message-box 'message)
-
-;;; 履歴の保存数を増やす
-(setq history-length 1000)
-
-;;; キーストロークをエコーエリアに早く表示する
-(setq echo-keystrokes 0.1)
-
-;;; 大きいファイルを開くときの警告の閾値を上げる
-(setq large-file-warning-threshold (* 25 1024 1024))
-
-;;; ミニバッファで入力を取り消しても履歴に残す
-(defadvice abort-recursive-edit (before minibuffer-save activate)
-  (when (eq (selected-window) (active-minibuffer-window))
-    (add-to-history minibuffer-history-variable (minibuffer-contents))))
---
+++ emacs/inits/miscellaneous.el
@@ -0,0 +1,29 @@
+;;; バックアップファイルを作らない
+(setq backup-inhibited t)
+
+;;; gcを減らす
+(setq gc-cons-threshold (* 10 gc-cons-threshold))
+
+;;; ログの記録行数を増やす
+(setq message-log-max 10000)
+
+;;; ミニバッファを再帰的に呼び出せるようにする
+(setq enable-recursive-minibuffers t)
+
+;;; ダイアログボックスを使わないようにする
+(setq use-dialog-box nil)
+(defalias 'message-box 'message)
+
+;;; 履歴の保存数を増やす
+(setq history-length 1000)
+
+;;; キーストロークをエコーエリアに早く表示する
+(setq echo-keystrokes 0.1)
+
+;;; 大きいファイルを開くときの警告の閾値を上げる
+(setq large-file-warning-threshold (* 25 1024 1024))
+
+;;; ミニバッファで入力を取り消しても履歴に残す
+(defadvice abort-recursive-edit (before minibuffer-save activate)
+  (when (eq (selected-window) (active-minibuffer-window))
+    (add-to-history minibuffer-history-variable (minibuffer-contents))))
