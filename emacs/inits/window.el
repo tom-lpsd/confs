7daacb63415ceb8775c95d14758caabd2c771d85
Tom Tsuruhara <tom.lpsd@gmail.com>
2010-08-06 02:14:38 +0900

    C-x o ==> C-t

--- emacs/inits/window.el
+++ emacs/inits/window.el
@@ -2,4 +2,11 @@
   (interactive)
   (recenter 0))
 
-(global-set-key (kbd "C-z") 'line-to-top-of-window)
+(global-set-key (kbd "C-z") #'line-to-top-of-window)
+
+(defun other-window-or-split ()
+  (interactive)
+  (when (one-window-p) (split-window-vertically))
+  (other-window 1))
+
+(global-set-key (kbd "C-t") #'other-window-or-split)
