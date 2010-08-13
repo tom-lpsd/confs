30f71877a78f570cfa08cd69c0c722d6d0b45a56
Tom Tsuruhara <tom.lpsd@gmail.com>
2010-08-13 15:37:31 +0900

    added cperl-mode setting

--- emacs/inits/programming.el
+++ emacs/inits/programming.el
@@ -1,1 +1,10 @@
 (require 'clojure-mode)
+
+(defalias 'perl-mode 'cperl-mode)
+
+(setq auto-mode-alist
+      (append '(("\\.plx$" . perl-mode)
+		("\\.cgi$" . perl-mode)
+		("\\.pfc$" . lisp-mode)
+		("\\.oct$" . octave-mode)
+                ) auto-mode-alist))
