72b15e4cffde78575c35415a0b939514aa87294a
Tom Tsuruhara <tom.lpsd@gmail.com>
2010-08-15 17:34:05 +0900

    introducing yasnippets.el

---
+++ emacs/inits/yasnippets-setting.el
@@ -0,0 +1,4 @@
+;; (install-elisp-from-emacswiki "yasnippet-config.el")
+(add-to-list 'load-path "~/.emacs.d/plugins/yasnippet-0.6.1c")
+(require 'yasnippet-config)
+(yas/setup "~/.emacs.d/plugins/yasnippet-0.6.1c")
