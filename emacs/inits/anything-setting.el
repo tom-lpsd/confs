f9c3199d8ada67b5917472bda33c2cc025ba07fb
Tom Tsuruhara <tom.lpsd@gmail.com>
2010-08-17 15:43:28 +0900

    run anything from C-]. C-x b => anything-buffers+

--- emacs/inits/anything-setting.el
+++ emacs/inits/anything-setting.el
@@ -1,3 +1,4 @@
 (require 'anything-startup)
-(global-set-key (kbd "C-x b") #'anything)
+(global-set-key (kbd "C-]") #'anything)
+(global-set-key (kbd "C-x b") #'anything-buffers+)
 (define-key anything-map (kbd "C-h") 'delete-backward-char)
