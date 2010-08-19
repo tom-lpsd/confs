5a34947a0871eb42bb06e77487a3a864fdec2ab0
Tom Tsuruhara <tom.lpsd@gmail.com>
2010-08-19 13:33:27 +0900

    added yaml-mode

--- emacs/inits/programming.el
+++ emacs/inits/programming.el
@@ -29,3 +29,8 @@
                 ) auto-mode-alist))
 
 (setq lisp-indent-offset 2)
+
+;; (install-elisp "http://github.com/yoshiki/yaml-mode/raw/master/yaml-mode.el")
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
