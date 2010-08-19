5fa2a223d01699bf033b6459c24bd9df38432b85
Tom Tsuruhara <tom.lpsd@gmail.com>
2010-08-19 13:40:25 +0900

    C-M-p and C-M-n is enough to use...

--- emacs/inits/cursor.el
+++ emacs/inits/cursor.el
@@ -13,16 +13,3 @@
     (backward-kill-word 1)))
 
 (define-key global-map (kbd "C-w") #'kill-backward-dwim)
-
-;; C-(, C-)で対応括弧にジャンプ
-(defun move-to-match-parenthesis (arg)
-  "Go to the matching parenthesis if on parenthesis otherwise insert %."
-  (interactive "p")
-  (if (looking-at-p "\\s(")
-    (forward-list)
-    (unless (bobp)
-      (when (save-excursion (backward-char) (looking-at-p "\\s)"))
-	  (backward-list)))))
-
-(global-set-key (kbd "C-(") 'move-to-match-parenthesis)
-(global-set-key (kbd "C-)") 'move-to-match-parenthesis)
