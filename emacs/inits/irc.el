01133e4a88afe0922aaba9e991645e31fe69b967
Tom Tsuruhara <tom.lpsd@gmail.com>
2010-09-08 10:47:03 +0900

    use C-cC-k as riece-command-previous-channel

--- emacs/inits/irc.el
+++ emacs/inits/irc.el
@@ -1,1 +1,5 @@
 (autoload 'riece "riece" "IRC client" t)
+(add-hook 'riece-command-mode-hook
+	  (lambda ()
+	    (define-key riece-command-mode-map
+	      (kbd "C-c C-k") 'riece-command-previous-channel)))
