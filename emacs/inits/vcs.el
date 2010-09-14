4ef3f348ae1746eb5ae40bc291d2733707de28b6
Tom Tsuruhara <tom.lpsd@gmail.com>
2010-09-14 14:48:51 +0900

    install egg.el

--- emacs/inits/vcs.el
+++ emacs/inits/vcs.el
@@ -1,3 +1,7 @@
 ;; (auto-install-from-url "http://svn.apache.org/repos/asf/subversion/trunk/contrib/client-side/emacs/dsvn.el")
 (autoload 'svn-status "dsvn" "Run `svn status'." t)
 (autoload 'svn-update "dsvn" "Run `svn update'." t)
+
+;; (install-elisp "http://github.com/byplayer/egg/raw/master/egg.el")
+(when (executable-find "git")
+  (require 'egg nil t))
