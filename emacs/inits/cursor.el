a02b2a23da72121c5530935d891341d89e85413d
Tom Tsuruhara <tom.lpsd@gmail.com>
2011-01-28 10:51:34 +0900

    find the empty line

--- emacs/inits/cursor.el
+++ emacs/inits/cursor.el
@@ -13,3 +13,20 @@
     (backward-kill-word 1)))
 
 (define-key global-map (kbd "C-w") #'kill-backward-dwim)
+
+(defun move-to-next-empty-line ()
+  (interactive)
+  (let ((next (save-excursion
+		(forward-char)
+		(search-forward-regexp "^$" nil t))))
+    (when next (goto-char next))))
+
+(defun move-to-prev-empty-line ()
+  (interactive)
+  (let ((prev (save-excursion
+		(backward-char)
+		(search-backward-regexp "^$" nil t))))
+    (when prev (goto-char prev))))
+
+(define-key global-map (kbd "C->") #'move-to-next-empty-line)
+(define-key global-map (kbd "C-<") #'move-to-prev-empty-line)
