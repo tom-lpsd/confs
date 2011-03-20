a3e15e96d38aceb3f6bf6cfa5036eb4730539f36
Tom Tsuruhara <tom.lpsd@gmail.com>
2011-03-20 22:35:41 +0900

    change font setting for cocoa emacs.

--- emacs/darwin/appearance.el
+++ emacs/darwin/appearance.el
@@ -7,11 +7,29 @@
 		(inactive-alpha . 0.50)) default-frame-alist))
 
 (setq initial-frame-alist
-      '((top . 0) (left . 0) (width . 238) (height . 60)
-	(font . "-apple-monaco-medium-r-normal--13-130-72-72-m-130-iso10646-1")))
+      '((top . 0) (left . 0) (width . 238) (height . 60)))
 
 (when (>= emacs-major-version 23)
-  (set-fontset-font
-   (frame-parameter nil 'font)
-   'japanese-jisx0208
-   '("Hiragino Maru Gothic Pro" . "iso10646-1")))
+ (set-face-attribute 'default nil
+                     :family "monaco"
+                     :height 140)
+ (set-fontset-font
+  (frame-parameter nil 'font)
+  'japanese-jisx0208
+  '("Hiragino Maru Gothic Pro" . "iso10646-1"))
+ (set-fontset-font
+  (frame-parameter nil 'font)
+  'japanese-jisx0212
+  '("Hiragino Maru Gothic Pro" . "iso10646-1"))
+ (set-fontset-font
+  (frame-parameter nil 'font)
+  'mule-unicode-0100-24ff
+  '("monaco" . "iso10646-1"))
+ (setq face-font-rescale-alist
+      '(("^-apple-hiragino.*" . 1.2)
+        (".*osaka-bold.*" . 1.2)
+        (".*osaka-medium.*" . 1.2)
+        (".*courier-bold-.*-mac-roman" . 1.0)
+        (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
+        (".*monaco-bold-.*-mac-roman" . 0.9)
+        ("-cdac$" . 1.3))))
