ecc9c9682e45947a791bdcc699076c19d5e24701
Tom Tsuruhara <tom.lpsd@gmail.com>
2013-03-31 15:30:29 +0900

    Added font config for mac

--- emacs/darwin.el
+++ emacs/darwin.el
@@ -1,3 +1,16 @@
 (setq mac-pass-control-to-system nil)
 (setq mac-command-key-is-meta t)
 (setq ns-command-modifier 'meta)
+
+(dolist (target '(japanese-jisx0208 japanese-jisx0212))
+  (set-fontset-font nil target
+		    '("Hiragino Maru Gothic Pro" . "iso10646-1")))
+
+(setq face-font-rescale-alist
+      '(("^-apple-hiragino.*" . 1.2)
+        (".*osaka-bold.*" . 1.2)
+        (".*osaka-medium.*" . 1.2)
+        (".*courier-bold-.*-mac-roman" . 1.0)
+        (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
+        (".*monaco-bold-.*-mac-roman" . 0.9)
+        ("-cdac$" . 1.3)))
